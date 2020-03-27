fff8350359be2b52605fe20a1239d3760c4d389f
bibazavr <lirik.hariev@gmail.com>
2020-03-27 19:35:54 +0300

    del App-test.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
+// jest.config.js
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    preset: "react-native",
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js"
+    ],
+    transform: {
+        "^.+\\.(js)$": "<rootDir>/node_modules/babel-jest",
+        "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    testPathIgnorePatterns: [
+        "\\.snap$",
+        "<rootDir>/node_modules/"
+    ],
+    cacheDirectory: ".jest/cache"
 };
