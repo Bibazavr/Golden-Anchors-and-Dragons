4dbc8f231ea6c1c9e8ef919df94fae7e293b5f1a
bibazavr <lirik.hariev@gmail.com>
2020-04-15 21:15:28 +0300

    now checkbox works fine

--- screens/homeScreen/toDoScreen/TodoScreen.tsx
+++ screens/homeScreen/toDoScreen/TodoScreen.tsx
@@ -1,25 +1,43 @@
 import * as React from "react";
-import {Button, FlatList, TextInput, View} from "react-native";
-import {stylesTodoScreen} from "./stylesTodoScreen";
+import {Button, FlatList, StyleSheet, TextInput, View} from "react-native";
 import {TodoItem} from "./components/TodoItem";
-import {CheckBox} from "react-native-elements";
+import {onChangeTodoItemChecked} from "./utils/onChangeTodoItemChecked";
+
+
+export interface TodoScreenState {
+    todoItem: { title: string, key: string, checked: boolean }
+    list: TodoScreenState["todoItem"][]
+}
 
 export const TodoScreen = () => {
-    const [list, setList] = React.useState<{ title: string, key: string }[]>([])
+    const [list, setList] = React.useState<TodoScreenState["list"]>([])
     const [todoItem, setTodoItem] = React.useState<string>("");
-    console.log(todoItem, list);
+
     return <View style={stylesTodoScreen.container}>
         <TextInput style={stylesTodoScreen.input}
                    onChangeText={(inputText) => setTodoItem(inputText)}
                    placeholder={"Введите todo"}/>
-        <Button title={"add"} onPress={() => {
+
+        <Button title={"add"} disabled={todoItem.length === 0} onPress={() => {
             let newValue = list.slice()
-            newValue.push({title: todoItem, key: list.length.toString()})
+            newValue.push({title: todoItem, key: list.length.toString(), checked: false})
             setList(newValue)
         }}/>
-        <CheckBox checked={false} title={"BIBA"}/>
 
-        <FlatList data={list} renderItem={({item}) => <TodoItem name={item.title} checked={true}/>}/>
+        <FlatList data={list} renderItem={({item}) => <TodoItem name={item.title}
+                                                                checked={item.checked}
+                                                                onChangeCheck={() => setList(onChangeTodoItemChecked(list, item.key))}/>}/>
     </View>
 }
 
+const stylesTodoScreen = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    input: {
+        backgroundColor: "#f3f3f3",
+        height: 30,
+        margin: 10
+    },
+});
--- screens/homeScreen/toDoScreen/components/TodoItem.tsx
+++ screens/homeScreen/toDoScreen/components/TodoItem.tsx
@@ -2,13 +2,20 @@ import * as React from "react";
 import {StyleSheet, View} from "react-native";
 import {CheckBox} from 'react-native-elements'
 
+
+interface TodoItemProps {
+    name: string
+    checked: boolean
+    onChangeCheck: () => void;
+}
+
 export const TodoItem = (props: TodoItemProps) => {
     return <View style={stylesTodoItem.container}>
-        <CheckBox checked={true} title={props.name}/>
+        <CheckBox checked={props.checked} title={props.name} onPress={props.onChangeCheck}/>
     </View>
 }
 
-export const stylesTodoItem = StyleSheet.create({
+const stylesTodoItem = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
@@ -19,8 +26,3 @@ export const stylesTodoItem = StyleSheet.create({
         width: 50
     }
 });
-
-export interface TodoItemProps {
-    name: string
-    checked: boolean
-}
