5b16d7f6dc9d91bdf0f081a459f2510d7592110e
bibazavr <lirik.hariev@gmail.com>
2020-04-26 16:27:19 +0300

    setup inventory

--- screens/homeScreen/inventoryScreen/stylesInventoryScreen.ts
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from "react-native";
-
-
-export const stylesInventoryScreen = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-});
